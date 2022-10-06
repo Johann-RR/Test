d15eef9c67a17059fa9391f07ffb7de40c73a47f
Johann-RR <johannreis2006@gmail.com>
2022-10-06 09:27:42 -0300

    test

--- index2.php
+++ index2.php
@@ -1,11 +1,9 @@
-   <?php
+<?php
     include_once'./includes/_header.php';
     ?>
-
-    <main>
-        <h1> ovo alface</h1>
+<main>
+        <h1> banana maça</h1>
     </main>
-
     <?php
     include_once'./includes/aside.php';
     include_once'./includes/footer.php';
